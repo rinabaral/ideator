bb44dafb19d66f87f739399fd9b1e2c8e8d2f4d1
rinabaral <rina12baral@gmail.com>
2016-09-08 17:45:24 +0545

    idea editing added

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -6,7 +6,17 @@ end
     @idea = Idea.create(idea_params)
     redirect_to root_path
   end
-
+  def edit
+  	@idea = Idea.find(params[:id])
+end
+def update
+  @idea = Idea.find(params[:id])
+  if @idea.update(idea_params)
+    redirect_to root_path
+  else
+    redirect_to edit_idea_path(params[:id])
+  end
+end
   private
 
   def idea_params
